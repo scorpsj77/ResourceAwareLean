313a71acd739e6302faf208b8e71633af510edc2
differentialplantain <143018613+differentialplantain@users.noreply.github.com>
2026-08-13 11:24:26 -0700

    Add ResourceModel import to ResourceModel.lean

--- TextbookAlgorithms/KleinbergTardos/Chapter04/Dijkstra/ResourceModel.lean
+++ TextbookAlgorithms/KleinbergTardos/Chapter04/Dijkstra/ResourceModel.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dayakumaran Ramalingam
 -/
 import TextbookAlgorithms.KleinbergTardos.Chapter04.Dijkstra.Algorithm
+import ResourceAware.Foundations.Graph.ResourceModel
 import ResourceAware.Foundations.PriorityQueue.ResourceModel
 import ResourceAware.Program.Cost
 
